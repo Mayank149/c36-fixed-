a46865d969337be141f5db64c649d9ea70c27e53
Mayank149 <73521432+Mayank149@users.noreply.github.com>
2021-01-08 17:19:13 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var canvas,backgroungimage;
+var gameState = 0;
+var playerCount,database;
+var form,player,game;
+
+function setup(){
+
+canvas=createCanvas(500,500);
+database = firebase.database();
+game=new Game();
+game.getState();
+
+game.start();
+
+}
+
+function draw(){
+
+
+
+
+}
